ae4ed84705c1aa288b31d385c298311597ebe32f
Molotow5 <v_spassov@web.de>
2019-04-10 17:30:46 +0200

    Hat funktioniert. Bist 1.11

---
+++ source/Aufgabe1_11.cpp
@@ -0,0 +1,22 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include <cmath>
+
+double fract(double zahl){
+    if (zahl < 1 && zahl > -1){
+        return zahl;
+    }
+    int vorKommaAnteil = zahl;
+    return zahl - vorKommaAnteil;
+}
+
+TEST_CASE("describe_gcd", "[gcd]"){
+    REQUIRE(0.5 == Approx(fract(2.5)));
+    REQUIRE(Approx(0.2) == 0.2);
+    REQUIRE(Approx(fract(-24.245)) == -0.245);
+}
+
+int main(int argc, char* argv[])
+{
+  return Catch::Session().run(argc, argv);
+}
