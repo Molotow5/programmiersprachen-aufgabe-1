b29105eb3d39b3a70f097eda8223a21ca1383752
Molotow5 <v_spassov@web.de>
2019-04-14 22:35:59 +0200

    Added Aufgabe 1.13

---
+++ source/aufgabe1_13.cpp
@@ -0,0 +1,34 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include <cmath>
+
+int factorial(int zahl){
+    int ergebnis;
+    if (zahl == 0){
+        ergebnis = 0;
+    } else {
+        ergebnis = 1;
+    }
+    if (zahl > 0){
+        for (int i = 1; i < 1+zahl; ++i){
+            ergebnis = ergebnis * i;
+        }
+    } else {
+        for (int j = -1; j > zahl-1; j--){
+            ergebnis = ergebnis * j;
+        }
+    }
+    return ergebnis;
+}
+
+TEST_CASE("describe_gcd", "[gcd]"){
+    REQUIRE( factorial(5) == 120);
+    REQUIRE( factorial(8) == 40320);
+    REQUIRE( factorial(0) == 0);
+    REQUIRE( factorial(-11) == -39916800);
+}
+
+int main(int argc, char* argv[])
+{
+  return Catch::Session().run(argc, argv);
+}
