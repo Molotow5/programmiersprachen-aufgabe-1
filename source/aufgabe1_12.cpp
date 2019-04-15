f9091ccf36eff59c95e9bf69b7745a9d4a9b2bf9
Molotow5 <v_spassov@web.de>
2019-04-15 17:44:34 +0200

    Korrektur, wieder mal

--- source/aufgabe1_12.cpp
+++ source/aufgabe1_12.cpp
@@ -4,12 +4,18 @@
 #include <cmath>
 
 double zylinder_Vol(double height, double radius){
+    if (height < 0 || radius < 0){
+        return 0;
+    }
     double pi = M_PI;
     double volume = radius * radius * pi * height;
     return volume;
 }
 
 double zylinder_Ao(double height, double radius){
+        if (height < 0 || radius < 0){
+        return 0;
+    }
     double pi = M_PI;
     double surface = (radius * radius * pi) * 2 + (radius * 2 * pi) * height;
     return surface;
@@ -20,6 +26,8 @@ TEST_CASE("describe_zylinder_Vol&Ao", "[zylinder_Vol&Ao]"){
     REQUIRE(Approx(zylinder_Vol(10, 15)).epsilon(0.01) == 7069.58);
     REQUIRE(Approx(zylinder_Ao(25, 5)) == 942.48);
     REQUIRE(Approx(zylinder_Ao(10, 15)).epsilon(0.01) == 2356.19);
+    REQUIRE(zylinder_Vol(-5, 30) == 0);
+    REQUIRE(zylinder_Ao(20, -1) == 0);
 }
 
 int main(int argc, char* argv[])
